c56c1d11b5285a4ee6c1835028dc3a3027027dc9
Ankush263 <ankushbanik263@gmail.com>
2024-06-16 17:30:03 +0530

    Add catchAsync function for handle asynchronous functions

---
+++ server/src/utils/catchAsync.js
@@ -0,0 +1,9 @@
+const catchAsync = (asyncFunction) => {
+	return (req, res, next) => {
+		asyncFunction(req, res, next).catch((err) => {
+			next(err);
+		});
+	};
+};
+
+module.exports = catchAsync;
