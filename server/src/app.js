0def212c302f16484ee05f92dc2cb3596c4578a1
Ankush263 <ankushbanik263@gmail.com>
2024-06-16 19:42:43 +0530

    Add router and error handler

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const globalErrorHandler = require('./middlewares/global-error');
+const router = require('./routes/index');
 
 const app = express();
 
@@ -9,5 +11,9 @@ app.use(cors());
 
 app.use(cookieParser());
 app.use(bodyParser.json());
+
+app.use('/api/v1', router);
+
+app.use(globalErrorHandler);
 
 module.exports = app;
