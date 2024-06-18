b20955f3565f94d0e9a3dcc2c1b431b82808a437
Ankush263 <ankushbanik263@gmail.com>
2024-06-18 20:31:03 +0530

    Add signup functionality

--- client/src/actions/sign-up.ts
+++ client/src/actions/sign-up.ts
@@ -1,67 +1,38 @@
 'use server';
 
 import { AxiosResponse } from 'axios';
-import { z } from 'zod';
-import { signup } from '../api';
+import { signup } from '../app/api';
 import { redirect } from 'next/navigation';
-
-interface SignUpFormState {
-	errors?: {
-		username?: string[];
-		email?: string[];
-		password?: string[];
-		_form?: string[];
-	};
-	success?: boolean;
-	data?: string;
-}
-
-const SignupSchema = z.object({
-	username: z
-		.string()
-		.min(4, { message: 'username must be at least 4 characters long.' })
-		.trim(),
-	email: z.string().email({ message: 'Please enter the valid email' }).trim(),
-	password: z
-		.string()
-		.min(6, { message: 'Password must be atleast 6 characters long' })
-		.regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
-		.regex(/[0-9]/, { message: 'Contain at least one number.' })
-		.trim(),
-});
+import { cookies } from 'next/headers';
 
 export async function signupMethod(
-	formState: SignUpFormState,
-	formData: FormData
-): Promise<SignUpFormState> {
-	console.log('validatedFields');
-
-	const validatedFields = SignupSchema.safeParse({
-		username: formData.get('username'),
-		email: formData.get('email'),
-		password: formData.get('password'),
-	});
-
-	if (!validatedFields.success) {
-		console.log('error: ', validatedFields.error.flatten().fieldErrors);
-		return {
-			errors: validatedFields.error.flatten().fieldErrors,
-		};
-	}
-
+	username: string,
+	email: string,
+	password: string
+) {
 	let user: AxiosResponse;
 
 	try {
-		await signup({
-			username: validatedFields.data.username,
-			email: validatedFields.data.email,
-			password: validatedFields.data.password,
+		user = await signup({
+			username,
+			email,
+			password,
+		});
+
+		const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+		cookies().set('Token', user?.data?.token as string, {
+			httpOnly: true,
+			secure: true,
+			expires: expiresAt,
+			sameSite: 'lax',
+			path: '/',
 		});
 	} catch (error: any) {
 		return {
 			success: false,
-			errors: {
-				_form: error?.response?.data?.message,
+			error: {
+				message: error?.response?.data?.message,
 			},
 		};
 	}
