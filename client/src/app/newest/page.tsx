1297c972b91d720179e3e090f0047ec3f966fffd
Ankush263 <ankushbanik263@gmail.com>
2024-06-20 01:12:12 +0530

    update pages

--- client/src/app/newest/page.tsx
+++ client/src/app/newest/page.tsx
@@ -1,13 +1,22 @@
 import ServerHeader from '@/components/headers/serverHeader';
 import SubmitHeader from '@/components/headers/submitHeader';
 import Post from '@/components/posts/all-posts';
+import { getToken } from '@/lib/getToken';
 
-export default function New() {
+export default async function New() {
+	const token = await getToken();
+
 	return (
 		<div className="flex w-[100%] flex-col items-center">
 			<ServerHeader />
-			<SubmitHeader />
-			<div className="w-[90%] border-b-2 border-gray-700 mb-5 mt-5"></div>
+			{token ? (
+				<>
+					<SubmitHeader />
+					<div className="w-[90%] border-b-2 border-gray-700 mb-5 mt-5"></div>
+				</>
+			) : (
+				''
+			)}
 			<div className="w-[85%]">
 				<Post type="new" />
 			</div>
