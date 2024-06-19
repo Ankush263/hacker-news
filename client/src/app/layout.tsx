5343327c38d4cae358eeaa4865a3d6d829dc8c8f
Ankush263 <ankushbanik263@gmail.com>
2024-06-20 01:11:35 +0530

    update

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import { cn } from '@/lib/utils';
+import './globals.css';
 
 const fontSans = FontSans({
 	subsets: ['latin'],
