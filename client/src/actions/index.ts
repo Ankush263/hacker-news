dd4266862667a55cd2489a2dfcac0ee9a6fd7c3b
Ankush263 <ankushbanik263@gmail.com>
2024-06-20 22:39:13 +0530

    Not Needed

--- client/src/actions/index.ts
+++
@@ -1,8 +0,0 @@
-export { signupMethod } from './sign-up';
-export { loginMethod } from './log-in';
-export { logoutMethod } from './log-out';
-export { postSubmitMethod } from './submit';
-export { createCommentMethod } from './create-comment';
-export { deletePostMethod } from './delete-post';
-export { updatePostMethod } from './update-post';
-export { AddScoreMethod } from './add-score';
